622537071914821e9a5cff968dd6434438812bb3
Herihaja <hery.imiary@gmail.com>
2022-05-11 16:06:26 +0300

    Herihaja | enhancing progressively edit entity page

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,11 +12,13 @@ import '../styles/app.css';
 import '../bootstrap';
 
 
-import React from 'react';
+import React, {StrictMode} from 'react';
 import Fields from './components/Fields';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-//import reportWebVitals from "./reportWebVitals";
+import { Modal, Button, Form } from "react-bootstrap";
+import Select from "react-select";
+import "bootstrap/dist/css/bootstrap.css";
+
 
  class Entity extends React.Component {
     constructor() {
@@ -24,15 +26,14 @@ import { createRoot } from 'react-dom/client';
 
         this.state = {
             entries: [],
-            name: 'entity default',
+            entityName: 'entity default',
             description: 'Lorem ipsum',
+            showAddField: false,
+            editedField: {'name': 'akondro', 'type': {'value': 'date', 'label':  'Date'}, 'id':0, 'constraints': 'max=100'}
         };
     }
 
     componentDidMount = () => {
-        //this.setState({entries: [{'id': 23, 'name':'id', 'type':'integer', 'constraints':'max=10'}]});
-        //this.setState({entries: [["23",'id', 'integer', 'max=10']]});
-        
         fetch('https://localhost:8000/entity-fields/')
             .then(response => response.json())
             .then(entries => {
@@ -43,52 +44,102 @@ import { createRoot } from 'react-dom/client';
     }
 
     changeEntityName = (e) => {
-        this.setState({name: e.target.value});
+        this.setState({entityName: e.target.value});
     }
 
     clickDeleteField = (id) => {
-        this.setState({entries: this.state.entries.filter(item => item.id !== id)})
+        var entries = this.state.entries;
+        entries.splice(id, 1);
+        this.setState({entries: entries});
+    }
+
+    addField = () => {
+        if (!this.state.showAddField)
+        this.setState({showAddField: true});
+        else
+        this.setState({showAddField: false});
+    }
+
+    closeFieldModal = () => {
+        this.setState({showAddField: false});
+    }
+
+    updateEditedFieldType = (selected) => {
+        var editedField = this.state.editedField;
+        editedField.type = selected;
+        this.setState(editedField);
+    }
+
+    updateEditedFieldName = (e) => {
+        var editedField = this.state.editedField;
+        editedField.name = e.target.value;
+        this.setState({'editedField': editedField});
+    }
+
+    fieldTypeOptions = [
+        {'value':'text', 'label': 'Text'}, 
+        {'value':'number', 'label': 'Number'}, 
+        {'value':'date', 'label': 'Date'}, 
+        {'value':'relation', 'label':'Relation'},
+    ];
+
+    saveFieldForm = () => {
+        var entries = this.state.entries; 
+        var edited = JSON.parse(JSON.stringify(this.state.editedField));
+        entries.push(edited);
+        this.setState({'entries': entries, 'showAddField': false})
     }
 
     
     render () { 
         return (
             <div className="entity-container">
-                <input type="text" value={this.state.name} onChange={this.changeEntityName} /> 
+                <input type="button" onClick={this.addField} value="Add field"/>
+                <input type="text" value={this.state.entityName} onChange={this.changeEntityName} /> 
                 <textarea name="description" defaultValue={this.state.description}></textarea>
+
                 {this.state.entries.map(
-                     ({ id, name, type, constraints }) => (
+                     ({ id, name, type, constraints }, index) => (
                         <Fields
-                        key={id}
+                        key={index+1}
+                        index={index+1}
                         id={id}
                         name={name}
-                        type={type}
+                        type={type.value}
                         constraints={constraints}
                         clickDelete={this.clickDeleteField}
                     >
                     </Fields>
                      )
                  )}
+                <StrictMode>
+                <Modal show={this.state.showAddField} animation={true} onHide={this.closeFieldModal} backdrop="static">
+
+                    <Modal.Header closeButton>
+                        <Modal.Title>Field Form</Modal.Title>
+                    </Modal.Header>
+            
+                    <Modal.Body>
+                        <label>Name: </label><input type="text" value={this.state.editedField.name}  onChange={this.updateEditedFieldName}/> 
+                        <br/>
+                        <label>Type: </label>
+                        <Select defaultValue={this.state.editedField.type} value={this.state.editedField.type} onChange={this.updateEditedFieldType} options={this.fieldTypeOptions}>
+                        </Select>
+                        
+                    </Modal.Body>
+            
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.saveFieldForm}>Secondary</Button>
+                        <Button variant="primary" onClick={this.saveFieldForm}>Primary</Button>
+                    </Modal.Footer>
+            
+                </Modal>
+                </StrictMode>
             </div>
         );
     }
     
 }
 
-//ReactDOM.render(<App />, document.getElementById('fields-list'));
-
 const rootElement = document.getElementById("fields-list");
-const root = createRoot(rootElement).render(<Entity />);
-/*
-ReactDOM.render(
-    <App />, rootElement
-);
-
-/*
-createRoot(
-    document.getElementById('fields-list')
-).render(
-<App />
-)
-
-//reportWebVitals();*/
+createRoot(rootElement).render(<Entity />);
--- assets/js/components/Fields.js
+++ assets/js/components/Fields.js
@@ -1,24 +1,22 @@
     // ./assets/js/components/Fields.js
     import React from 'react';
      
-    const Fields = ({ id, name, type, constraints, clickDelete }) => {
-        /*const clickDelete = (id) => {
-            console.log(id, "from child");
-        }*/
+    const Fields = ({ id, name, type, constraints, clickDelete , index}) => {
+        
 
         return (
             <div className="row">
                 <div key={id} className="col-2">
-                    <p>{id}</p>
+                    <p>{index} ({id})</p>
                 </div>
                 <div className="col-4">
                     {name}
                 </div>
                 <div className="col-4">
-                    {type}
+                    {type} - {constraints}
                 </div>
                 <div className="col-2">
-                    <a href="#" onClick={ () => clickDelete(id)}> Delete</a>
+                    <a href="#" onClick={ () => clickDelete(index)}> Delete</a>
                 </div>
             </div>
         );
